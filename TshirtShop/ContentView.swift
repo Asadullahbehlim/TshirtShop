79ffd5dedacf254d9afa7b598a79cc3a26ea449a
Asadullah Behlim <asadullahbehlim@gmail.com>
2022-05-23 12:01:02 +0530

    Initial Commit

---
+++ TshirtShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TshirtShop
+//
+//  Created by Asadullah Behlim on 23/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TshirtShop/TshirtShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  TshirtShopApp.swift
+//  TshirtShop
+//
+//  Created by Asadullah Behlim on 23/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TshirtShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
